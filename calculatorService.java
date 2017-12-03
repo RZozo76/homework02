c1d42a82f3363d97f53c6ed5930a292063be2f92
RZozo76 <rossmannz@gmail.com>
2017-12-03 22:26:38 +0100

    2. calculator method ver-1

--- calculatorService.java
+++ calculatorService.java
@@ -1,19 +1,53 @@
 package hu.helixlab;
 
-public class calculatorService {
-    private boolean isItEven = true;
+import java.util.Scanner;
 
-    public boolean isItEven(int number) {
-        if ((number%2)== 0) {
-            isItEven = true;
-            System.out.println( " A szám " + number + " páros");
+public class calculatorService {
+    public static void main(String args[]) {
+        int x, y, operation;
+
+        System.out.println("1- összeadás");
+        System.out.println("2- kivonás");
+        System.out.println("3- osztás");
+        System.out.println("4- szorzás");
+        System.out.print("Válassz műveleti jelet -");
+        Scanner var = new Scanner(System.in);
+
+        operation = var.nextInt();
+        System.out.print("Add meg az első számot -");
+        x = var.nextInt();
+        System.out.print("Add meg a második számot -");
+        y = var.nextInt();
+        calc(x, y);
+        public static void calc(int x, int y) {
+            int c;
+
+            switch (operation) {
+                case 1:
+                    c = x + y;
+                    System.out.print("Output-" + c);
+                    break;
+
+                case 2:
+                    c = x - y;
+                    System.out.print("Output-" + c);
+                    break;
+
+                case 3:
+                    c = x / y;
+                    System.out.print("Output-" + c);
+                    break;
+
+                case 4:
+                    c = x * y;
+                    System.out.print("Output-" + c);
+                    break;
+            }
         }
-        else {
-            isItEven = false;
-            System.out.println( " A szám " + number + " nem páros");
-        }
-        return isItEven;
     }
+
+
+
 
 
 
