d91cff79ad4621c6e878097d6d76cc2febee6e06
RZozo76 <rossmannz@gmail.com>
2017-12-03 23:11:23 +0100

    3. calculatorHelper

---
+++ CalculationHelper.java
@@ -0,0 +1,46 @@
+package hu.helixlab;
+
+import java.util.Scanner;
+
+public class CalculationHelper {
+
+        public static void main(String[] args) {
+
+            Scanner reader = new Scanner(System.in);
+            System.out.println("Kérek két számot: ");
+
+            double first = reader.nextDouble();
+            double second = reader.nextDouble();
+
+            System.out.println("Kérek egy műveleti jelet (+, -, *, /): ");
+            char operator = reader.next().charAt(0);
+
+            double result;
+
+            switch(operator)
+            {
+                case '+':
+                    result = first + second;
+                    break;
+
+                case '-':
+                    result = first - second;
+                    break;
+
+                case '*':
+                    result = first * second;
+                    break;
+
+                case '/':
+                    result = first / second;
+                    break;
+
+                default:
+                    System.out.println("Hiba! nincs ilyen műveleti jel");
+                    return;
+            }
+
+            System.out.println(result);
+        }
+    }
+
