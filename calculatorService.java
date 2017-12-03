3c79a986c78b5a36116a17e17e04aa3ee1f5c9f1
RZozo76 <rossmannz@gmail.com>
2017-12-03 22:42:14 +0100

    2. calculator method ver-1

--- calculatorService.java
+++ calculatorService.java
@@ -3,52 +3,42 @@ package hu.helixlab;
 import java.util.Scanner;
 
 public class calculatorService {
-    public static void main(String args[]) {
-        int x, y, operation;
+    public static void main(String[] args) {
 
-        System.out.println("1- összeadás");
-        System.out.println("2- kivonás");
-        System.out.println("3- osztás");
-        System.out.println("4- szorzás");
-        System.out.print("Válassz műveleti jelet -");
-        Scanner var = new Scanner(System.in);
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Kérek két számot: ");
 
-        operation = var.nextInt();
-        System.out.print("Add meg az első számot -");
-        x = var.nextInt();
-        System.out.print("Add meg a második számot -");
-        y = var.nextInt();
-        calc(x, y);
-        public static void calc(int x, int y) {
-            int c;
+        double first = reader.nextDouble();
+        double second = reader.nextDouble();
 
-            switch (operation) {
-                case 1:
-                    c = x + y;
-                    System.out.print("Output-" + c);
-                    break;
+        System.out.println("Kérek egy műveleti jelet (+, -, *, /): ");
+        char operator = reader.next().charAt(0);
 
-                case 2:
-                    c = x - y;
-                    System.out.print("Output-" + c);
-                    break;
+        double result;
 
-                case 3:
-                    c = x / y;
-                    System.out.print("Output-" + c);
-                    break;
+        switch(operator)
+        {
+            case '+':
+                result = first + second;
+                break;
 
-                case 4:
-                    c = x * y;
-                    System.out.print("Output-" + c);
-                    break;
-            }
+            case '-':
+                result = first - second;
+                break;
+
+            case '*':
+                result = first * second;
+                break;
+
+            case '/':
+                result = first / second;
+                break;
+
+            default:
+                System.out.println("Hiba! nincs ilyen műveleti jel");
+                return;
         }
+
+        System.out.println(result);
     }
-
-
-
-
-
-
-}
+    }
